2b29d8a1ac83e18abe85b66db6c1837c8e7cc47c
Griselda <zeldagris82@gmail.com>
2019-03-18 20:52:17 -0700

    polished the game

--- Griselda/canvas-2/game.js
+++ Griselda/canvas-2/game.js
@@ -2,13 +2,31 @@ const context = canvas.getContext('2d');
 const player1 = new Player(0, 0, 100, 100, context);
 const bg = new Bg(0, 0, width + 1, height, context);
 const bg2 = new Bg(width-1, 0, width +1, height, context);
+let frame = 0;
+
+
+document.addEventListener (type: 'keydown', listener: (event) => {
+    
+   if(event.code === 'space'){
+       player1.moveUp();
+   }
+});
+
+
+
+
+
+
+
+
 function gameLoop(){
+    ++frame;
 
     // context.fillStyle = 'blue';
     // context.fillRect(0, 100, width, 100);
 
 
-    context.clearRect(0, 0, width, height);
+    context.clearRect(x = 0, y = 0, width, height);
     bg.update();
     bg2.update();
     bg.draw();
--- Griselda/canvas-2/player.js
+++ Griselda/canvas-2/player.js
@@ -14,6 +14,9 @@ class Player {
     sy = 80;
     sw = 80;
     sh = 70;
+    motion = 0;
+    ySpeed = -5;
+    SpriteFrame = 0;
 
     constructor(x,y,w,h, context){
 
@@ -32,18 +35,36 @@ class Player {
 
     draw() {
         this.context.drawImage(img, 
-            this.sx, this.sy, this.sw, this.sh,
+            this.sx, this.sy + (70 * this.spriteFrame), this.sw, this.sh,
             this.x, this,y, this.w, this.h
             );
     } 
 
     update(){
-        this.y += 1;
+        this.motion += 0.20;
+        this.x -= this.ySpeed + this.motion;
         this.y++;
         this.y = this.y + 1;
+        if(this.y > 520);
+            this.y = 520;
         
-        if(this.y > height){
-            this.y = -100;
         }
+       
+
+        if(this.motion > 10){
+            this.motion = 10;
+        }
+
+        if (this.motion > 1){
+            this.spriteFrame = 0;
+        }  else {
+            this.spriteFrame = 1;
+        }
+      }
+
+    moveUp(){
+        console.log('move up');
+        this.motion = 0;
+
     }
 }
